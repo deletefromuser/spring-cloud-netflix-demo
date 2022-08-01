d5f0e9af92bce1357fe3b5fe15c3eae78a24d028
zhang <mrzhangbin@outlook.com>
2022-08-01 23:11:52 +0800

    add circuitbreaker-resilience4j

---
+++ eureka-client-consumer/src/main/java/com/example/eurekaclientconsumer/controller/ExceptionController.java
@@ -0,0 +1,50 @@
+package com.example.eurekaclientconsumer.controller;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@EnableWebMvc
+public class ExceptionController extends ResponseEntityExceptionHandler {
+
+    /**
+     * handleException - Handles all the Exception recieving a request,
+     * responseWrapper.
+     * 
+     * @param request
+     * @param responseWrapper
+     * @return ResponseEntity<ResponseWrapper>
+     * @user ihuaylupo
+     * @since 2018-09-12
+     */
+    @ExceptionHandler(value = { Exception.class })
+    public @ResponseBody ResponseEntity<Map<String, String>> handleException(HttpServletRequest request) {
+
+        return ResponseEntity.ok(Map.of("mesage", "exception happened"));
+    }
+
+    /**
+     * handleIOException - Handles all the Authentication Exceptions of the
+     * application.
+     * 
+     * @param request
+     * @param exception
+     * @return ResponseEntity<ResponseWrapper>
+     * @user ihuaylupo
+     * @since 2018-09-12
+     */
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, Object>> handleIOException(HttpServletRequest request, RuntimeException e) {
+        return ResponseEntity.ok(Map.of("status", HttpStatus.NOT_ACCEPTABLE, "message", e.getMessage()));
+    }
+
+}
--- eureka-client-consumer/src/main/java/com/example/eurekaclientconsumer/controller/TodoController.java
+++ eureka-client-consumer/src/main/java/com/example/eurekaclientconsumer/controller/TodoController.java
@@ -2,6 +2,8 @@ package com.example.eurekaclientconsumer.controller;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.TimeoutException;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -15,7 +17,11 @@ import org.springframework.web.client.RestTemplate;
 import com.example.eurekaclientconsumer.feignclient.TodoClient;
 import com.example.eurekaclientconsumer.model.Todo;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class TodoController {
 
     RestTemplate restTemplate;
@@ -44,7 +50,12 @@ public class TodoController {
 
     // use FeignClient
     @GetMapping("/todos/{id}")
-    public Todo getTodo(@PathVariable int id) {
+    @CircuitBreaker(name = "getTodoWithId")
+    public Todo getTodo(@PathVariable int id) throws TimeoutException {
+        if(id == 111) {
+            sleep();
+        }
+        randomlyRunLong();
         return todoClient.getTodo(id);
     }
 
@@ -52,5 +63,21 @@ public class TodoController {
     @GetMapping("/todos/length")
     public Map<String, Integer> configRefresh(@Value("${todo.list.length}") int length) {
         return Map.of("length", length);
+    }
+
+    private void randomlyRunLong() throws TimeoutException {
+        Random rand = new Random();
+        int randomNum = rand.nextInt(3) + 1;
+        if (randomNum == 3)
+            sleep();
+    }
+
+    private void sleep() throws TimeoutException {
+        try {
+            Thread.sleep(5000);
+            throw new java.util.concurrent.TimeoutException();
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+        }
     }
 }
