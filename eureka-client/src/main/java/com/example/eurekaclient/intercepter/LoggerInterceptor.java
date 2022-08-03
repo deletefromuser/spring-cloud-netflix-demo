f927871707a52afe7dbcce329b3359066775729a
deletefromuser <45934893+deletefromuser@users.noreply.github.com>
2022-08-03 16:17:42 +0800

    add intercepter

---
+++ eureka-client/src/main/java/com/example/eurekaclient/intercepter/LoggerInterceptor.java
@@ -0,0 +1,23 @@
+package com.example.eurekaclientconsumer.intercepter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LoggerInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler) throws Exception {
+
+        log.info("[preHandle][" + request + "]" + "[" + request.getMethod()
+                + "]" + request.getRequestURI());
+
+        return true;
+    }
+}
