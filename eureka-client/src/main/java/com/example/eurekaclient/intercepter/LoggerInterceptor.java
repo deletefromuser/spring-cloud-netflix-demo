8b53c4fe989b222e78cd3bf8049c8fd472b1ea69
deletefromuser <45934893+deletefromuser@users.noreply.github.com>
2022-08-04 12:38:27 +0800

    log service request header

--- eureka-client/src/main/java/com/example/eurekaclient/intercepter/LoggerInterceptor.java
+++ eureka-client/src/main/java/com/example/eurekaclient/intercepter/LoggerInterceptor.java
@@ -3,8 +3,10 @@ package com.example.eurekaclient.intercepter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import lombok.val;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -17,6 +19,14 @@ public class LoggerInterceptor implements HandlerInterceptor {
 
         log.info("[preHandle][" + request + "]" + "[" + request.getMethod()
                 + "]" + request.getRequestURI());
+        log.info("---------    Header start   --------");
+        var headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+
+            log.info(name + ": " + StringUtils.join(request.getHeaders(name).asIterator(), ", "));
+        }
+        log.info("---------    Header end   --------");
 
         return true;
     }
