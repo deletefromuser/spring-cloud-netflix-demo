d5127b6248d638489045a2770d7e535a4f4fe2d5
deletefromuser <45934893+deletefromuser@users.noreply.github.com>
2022-08-04 12:49:07 +0800

    log service request header

--- eureka-client/src/main/java/com/example/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/example/eurekaclient/EurekaClientApplication.java
@@ -2,20 +2,27 @@ package com.example.eurekaclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.eurekaclient.intercepter.LoggerInterceptor;
 
 @SpringBootApplication
-public class EurekaClientApplication {
+public class EurekaClientApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClientApplication.class, args);
 	}
 
 	@Bean
-	// @LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LoggerInterceptor());
 	}
 }
