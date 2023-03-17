5252982edbffac1b11769b58c2cdc1fa4c3023f8
deletefromuser <45934893+deletefromuser@users.noreply.github.com>
2023-03-17 16:09:46 +0800

    add  oauth2-client

--- eureka-client-consumer/src/main/java/com/example/eurekaclientconsumer/config/SecurityConfig.java
+++ eureka-client-consumer/src/main/java/com/example/eurekaclientconsumer/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.eurekaclientconsumer.config;
 
-import org.keycloak.adapters.springsecurity.authentication.KeycloakLogoutHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
