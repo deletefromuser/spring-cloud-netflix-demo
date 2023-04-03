8cadff412739fd7d891b587a1574fcadf83dbdb5
deletefromuser <45934893+deletefromuser@users.noreply.github.com>
2023-04-03 15:35:42 +0800

    try spring cloud stream

--- eureka-client/src/main/java/com/example/eurekaclient/controller/KafkaController.java
+++ eureka-client/src/main/java/com/example/eurekaclient/controller/KafkaController.java
@@ -4,13 +4,9 @@ import java.util.function.Function;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.stream.messaging.Processor;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.eurekaclient.model.Todo;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,17 +15,10 @@ import lombok.extern.slf4j.Slf4j;
 public class KafkaController {
 
     @Autowired
-    private Processor pipe;
-
-    @Autowired
     private Function<String, String> uppercase;
 
     @RequestMapping("/createKafkaMessage")
     public String createKafkaMessage(@RequestParam(required = false) String msg) {
-        pipe.input()
-                .send(MessageBuilder.withPayload(new Todo("This is my message"))
-                        .build());
-
         uppercase.apply(StringUtils.defaultIfBlank(msg, "createKafkaMessage"));
 
         return "Hello World!";
