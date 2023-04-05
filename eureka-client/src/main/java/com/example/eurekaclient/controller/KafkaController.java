edd79bcf70dd572b8a2931e966cf795cb4b5a4c9
deletefromuser <45934893+deletefromuser@users.noreply.github.com>
2023-04-05 19:12:10 +0800

    try spring cloud stream

--- eureka-client/src/main/java/com/example/eurekaclient/controller/KafkaController.java
+++ eureka-client/src/main/java/com/example/eurekaclient/controller/KafkaController.java
@@ -1,9 +1,14 @@
 package com.example.eurekaclient.controller;
 
+import java.time.LocalDateTime;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.binder.PollableMessageSource;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +22,13 @@ public class KafkaController {
     @Autowired
     private Function<String, String> uppercase;
 
+    @Autowired
+    private Supplier<LocalDateTime> date;
+
     @RequestMapping("/createKafkaMessage")
     public String createKafkaMessage(@RequestParam(required = false) String msg) {
         uppercase.apply(StringUtils.defaultIfBlank(msg, "createKafkaMessage"));
+        date.get();
 
         return "Hello World!";
     }
@@ -28,5 +37,17 @@ public class KafkaController {
     public String receiveKafkaMessage() {
 
         return "Hello World!";
+    }
+
+    @Autowired
+    private PollableMessageSource source;
+
+    @Scheduled(fixedDelay = 5_000)
+    public void poll() {
+        System.out.println("Polling...");
+        this.source.poll(m -> {
+            log.info(m.getPayload().toString());
+        }, new ParameterizedTypeReference<String>() {
+        });
     }
 }
