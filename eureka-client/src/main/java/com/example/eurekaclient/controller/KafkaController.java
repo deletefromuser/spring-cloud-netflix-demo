66d4dcce4a43b32e847d8eeb1c31cb60da31f711
deletefromuser <45934893+deletefromuser@users.noreply.github.com>
2023-04-07 10:26:56 +0800

    try spring cloud stream

--- eureka-client/src/main/java/com/example/eurekaclient/controller/KafkaController.java
+++ eureka-client/src/main/java/com/example/eurekaclient/controller/KafkaController.java
@@ -1,14 +1,17 @@
 package com.example.eurekaclient.controller;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+import org.apache.commons.lang.ObjectUtils;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.stream.binder.PollableMessageSource;
-import org.springframework.cloud.stream.binder.kafka.KafkaMessageChannelBinder;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,21 +34,37 @@ public class KafkaController {
     // @Autowired
     // @Qualifier("uppercase-out-0")
     // private MessageChannel uppercaseOut;
-    // @Autowired
-    // @Qualifier("barin")
-    // private MessageChannel uppercaseOut;
+    @Autowired(required = false)
+    @Qualifier("barin")
+    private MessageChannel uppercaseOut;
+    @Autowired(required = false)
+    @Qualifier("barin-in-0")
+    private MessageChannel uppercaseOut2;
+    @Autowired(required = false)
+    @Qualifier("barin-out-0")
+    private MessageChannel uppercaseOut3;
 
-    @Autowired
-    KafkaMessageChannelBinder kafkaMessageChannelBinder;
+    // @Autowired
+    // KafkaMessageChannelBinder kafkaMessageChannelBinder;
 
     @RequestMapping("/createKafkaMessage")
     public String createKafkaMessage(@RequestParam(required = false) String msg) {
         uppercase.apply(StringUtils.defaultIfBlank(msg, "createKafkaMessage"));
         date.get();
-        // uppercaseOut.send(org.springframework.integration.support.MessageBuilder
-        // .withPayload(msg).build());
 
-        log.info(kafkaMessageChannelBinder.getBindings() + "");
+        MessageChannel channel = (MessageChannel) ObjectUtils.defaultIfNull(uppercaseOut, uppercaseOut2);
+        if (channel != null) {
+            log.info("channel.send");
+            channel.send(org.springframework.integration.support.MessageBuilder
+                    .withPayload(msg).build());
+        }
+        if(uppercaseOut3 != null) {
+            log.info("channel.send");
+            uppercaseOut3.send(org.springframework.integration.support.MessageBuilder
+                    .withPayload(msg).build());
+        }
+
+        // log.info(kafkaMessageChannelBinder.getBindings() + "");
 
         return "Hello World!";
     }
